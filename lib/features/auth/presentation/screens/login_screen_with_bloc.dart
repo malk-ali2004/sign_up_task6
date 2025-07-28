d435177f464e227e9ffee88c2d0e9a99f535c388
malk-ali2004 <ma6661@fayoum.edu.eg>
2025-07-28 11:37:11 +0300

    first commit

--- lib/features/auth/presentation/screens/login_screen_with_bloc.dart
+++ lib/features/auth/presentation/screens/login_screen_with_bloc.dart
@@ -1,3 +1,58 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import '../../../../core/validator/validators.dart';
+// import '../bloc/login_bloc.dart';
+// import '../bloc/login_event.dart';
+// import '../bloc/login_state.dart';
+
+// class LoginScreenWithBloc extends StatelessWidget {
+//   const LoginScreenWithBloc({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return BlocProvider(
+//       create: (_) => LoginBloc(),
+//       child: BlocBuilder<LoginBloc, LoginState>(
+//         builder: (context, state) {
+//           final bloc = context.read<LoginBloc>();
+//           return Scaffold(
+//             appBar: AppBar(title: const Text('Login')),
+//             body: Padding(
+//               padding: const EdgeInsets.all(16.0),
+//               child: Form(
+//                 key: bloc.formKey,
+//                 child: Column(
+//                   children: [
+//                     TextFormField(
+//                       decoration: const InputDecoration(labelText: 'Email'),
+//                       validator: Validators.validateEmail,
+//                       onChanged: (value) => bloc.add(LoginEmailChanged(value)),
+//                     ),
+//                     const SizedBox(height: 10),
+//                     TextFormField(
+//                       decoration: const InputDecoration(labelText: 'Password'),
+//                       obscureText: true,
+//                       validator: Validators.validatePassword,
+//                       onChanged: (value) => bloc.add(LoginPasswordChanged(value)),
+//                     ),
+//                     const SizedBox(height: 20),
+//                     ElevatedButton(
+//                       onPressed: () {
+//                         bloc.add(LoginSubmitted());
+//                       },
+//                       child: const Text('Login'),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../core/validator/validators.dart';
@@ -22,6 +77,7 @@ class LoginScreenWithBloc extends StatelessWidget {
               child: Form(
                 key: bloc.formKey,
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
                     TextFormField(
                       decoration: const InputDecoration(labelText: 'Email'),
@@ -41,6 +97,13 @@ class LoginScreenWithBloc extends StatelessWidget {
                         bloc.add(LoginSubmitted());
                       },
                       child: const Text('Login'),
+                    ),
+                    const SizedBox(height: 12),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/signup');
+                      },
+                      child: const Text("Don't have an account? Sign up"),
                     ),
                   ],
                 ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+
 // import 'package:flutter/material.dart';
 // import 'package:flutter_bloc/flutter_bloc.dart';
 // import 'features/auth/presentation/screens/signup_screen.dart';
+// import 'features/auth/presentation/bloc/signup_bloc.dart';
 // import 'core/themes/app_themes.dart';
 
 // void main() {
@@ -9,24 +11,27 @@
 
 // class MyApp extends StatelessWidget {
 //   const MyApp({super.key});
+
 //   @override
 //   Widget build(BuildContext context) {
 //     return MaterialApp(
-//       title: 'ITI Task',
-//       debugShowCheckedModeBanner: false,
+//       title: 'Signup App',
 //       theme: AppThemes.lightTheme,
-//       home: const SignupScreen(),
+//       home: BlocProvider(
+//         create: (_) => SignupBloc(),
+//         child: const SignupScreen(),
+//       ),
 //     );
 //   }
 // }
 
 
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'features/auth/presentation/screens/login_screen_with_bloc.dart';
 import 'features/auth/presentation/screens/signup_screen.dart';
 import 'features/auth/presentation/bloc/signup_bloc.dart';
+import 'features/auth/presentation/bloc/login_bloc.dart';
 import 'core/themes/app_themes.dart';
 
 void main() {
@@ -39,12 +44,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Signup App',
+      title: 'Auth App',
       theme: AppThemes.lightTheme,
-      home: BlocProvider(
-        create: (_) => SignupBloc(),
-        child: const SignupScreen(),
-      ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => BlocProvider(
+              create: (_) => LoginBloc(),
+              child: const LoginScreenWithBloc(),
+            ),
+        '/signup': (context) => BlocProvider(
+              create: (_) => SignupBloc(),
+              child: const SignupScreen(),
+            ),
+      },
     );
   }
 }
+
+
+
+
+
+

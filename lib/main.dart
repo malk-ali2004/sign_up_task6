92f2db581bccf4a0b8f3939eea23a64978182f61
malk-ali2004 <ma6661@fayoum.edu.eg>
2025-07-28 11:40:42 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,4 @@
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'features/auth/presentation/screens/signup_screen.dart';
-// import 'features/auth/presentation/bloc/signup_bloc.dart';
-// import 'core/themes/app_themes.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Signup App',
-//       theme: AppThemes.lightTheme,
-//       home: BlocProvider(
-//         create: (_) => SignupBloc(),
-//         child: const SignupScreen(),
-//       ),
-//     );
-//   }
-// }
-
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
